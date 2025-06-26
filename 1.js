2abe402c7f9b77d96db93ff1054556077452a445
Yakash231 <yesafakash.com>
2025-06-26 08:21:00 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+console.log("hello")
+console.log("world");
+process.stdout.write("hello world\n");
+console.table({ a: 1, b: 2 });
+let name="Akash"
+let roll_no=22052607
+let bool=false
+console.log(`Name: ${name}, Roll No: ${roll_no}, Boolean: ${bool}`);
+let tea_types=["Taj","Lemon","Black","Oolong"]
+console.log("Tea Types:", tea_types);
+console.log(typeof tea_types);
