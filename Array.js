f91dbe7111723f47d171360fd5a6a5a185bc8557
Yakash231 <yesafakash.com>
2025-06-27 10:02:10 +0530

    third

---
+++ Array.js
@@ -0,0 +1,16 @@
+let arr=["greentea","blacktea","oolongtea","lemontea"];
+let first_tea=arr[0]
+console.log("First Tea:", first_tea);
+let city = ["Delhi", "Mumbai", "London", "Chennai"];
+let favourite_city = city[2];
+console.log("Favourite City:", favourite_city);
+console.log("Array:", arr);
+arr[2]= "masala chai";
+console.log("Updated Array:", arr)
+let visited = ["Delhi", "Mumbai", "London", "Chennai"];
+console.log("Visited Cities:", visited);
+visited.push("Bangalore");
+console.log("Visited Cities:", visited);
+visited.splice(2,2);
+console.log("Visited Cities after removal:", visited);
+
